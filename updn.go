10fc847f27d0119e7c56f19d266a823e762427e9
pastebt <maye1968@gmail.com>
2015-11-08 21:52:00 -0800

    combine up and dn

--- updn.go
+++ updn.go
@@ -6,7 +6,9 @@ import (
     "fmt"
     "path"
     "sort"
+    "html"
     "strings"
+    "net/url"
     "net/http"
 )
 
@@ -96,6 +98,63 @@ func (r Root)Open(name string) (http.File, error) {
 }
 
 
+func dirList(w http.ResponseWriter, r *http.Request, f http.File) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    dirs, err := f.Readdir(-1)
+    if err != nil { //|| len(dirs) == 0 {
+        return
+    }
+    hUpload(w, r, path.Join("./", r.RequestURI) + "/")
+    fmt.Fprintf(w, "<pre>\n")
+    for _, d := range dirs {
+        name := d.Name()
+        if d.IsDir() {
+            name += "/"
+        }
+        // name may contain '?' or '#', which must be escaped to remain
+        // part of the URL path, and not indicate the start of a query
+        // string or fragment.
+        url := url.URL{Path: name}
+        fmt.Fprintf(w, "<a href=\"%s\">%s</a>\n",
+                    url.String(), html.EscapeString(name)) //htmlReplacer.Replace(name))
+    }
+    fmt.Fprintf(w, "</pre>\n</body></html>")
+}
+
+
+type fileHandler struct {
+    root http.FileSystem
+}
+
+
+func (fh *fileHandler)ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    upath := r.URL.Path
+    if !strings.HasPrefix(upath, "/") {
+        upath = "/" + upath
+        r.URL.Path = upath
+    }
+    name := path.Clean(upath)
+    f, err := fh.root.Open(name)
+    if err != nil {
+        http.Error(w, "Something Wrong", http.StatusInternalServerError)
+        return
+    }
+    defer f.Close()
+
+    d, err1 := f.Stat()
+    if err1 != nil {
+        http.Error(w, "Something Wrong", http.StatusInternalServerError)
+        return
+    }
+
+   if d.IsDir() {
+        dirList(w, r, f)
+        return
+    }
+    http.ServeContent(w, r, d.Name(), d.ModTime(), f)
+}
+
+
 var fileList http.Handler
 
 
@@ -123,7 +182,9 @@ func main() {
                http.StripPrefix("/files/", http.FileServer(Root("./"))))
     //fileList = http.FileServer(Root("./"))
     fmt.Printf("serve http at %s\n", os.Args[1])
-    mux.HandleFunc("/", hUpdn)
+    //mux.HandleFunc("/", hUpdn)
+    fh := fileHandler{Root("./")}
+    mux.Handle("/", &fh)
     err := http.ListenAndServe(":" + os.Args[1], mux)
     if err != nil { panic (err) }
 }
